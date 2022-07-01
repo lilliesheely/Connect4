cd381b907c9ce8c37663b96881f574c618d0a508
lilliesheely <lilliesheely@gmail.com>
2022-07-01 14:02:10 -0700

    rename css file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 /*----- constants -----*/
+
+
+
 /*----- app's state (variables) -----*/
+// Look to the wireframe to find the key state variables. Look at the image and see what you will need to track the state (“status") of. 
+
+
 /*----- cached element references -----*/
+
+
+
 /*----- event listeners -----*/
+
+
+
 /*----- functions -----*/
