5bfed358e1beedbb8e5aeee6194a8d7c30319357
lilliesheely <lilliesheely@gmail.com>
2022-07-01 16:34:04 -0700

    Getting started on JS

--- js/main.js
+++ js/main.js
@@ -17,14 +17,42 @@ let gameStatus;
 
 
 /*----- event listeners -----*/
-
+// event listener for click to register what column the user clicks in. 
 
 
 /*----- functions -----*/
 init();
 
 function init() {
-    board = null; 
+    board = [
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+        [null, null, null, null, null, null]
+    ];
     turn = 1; 
     gameStatus = null;
+    render(); 
 }; 
+
+function render() {
+    renderMessage();
+    renderBoard();
+
+}
+
+function renderMessage() { 
+    if (gameStatus === null) {
+// create message with whose turn
+    } else if (gameStatus === 1) {
+// create winner message with the winner's name 
+    } else {
+// create message of tie game. 
+}
+
+function renderBoard() { 
+    // create a loop that goes through board array. 
+}
