893f42fd8b53881f16e82d01189cc2c6ea015cf2
lilliesheely <lilliesheely@gmail.com>
2022-07-03 12:49:02 -0700

    worked on handlechoice

--- js/main.js
+++ js/main.js
@@ -13,13 +13,11 @@
 
     // /*----- cached element references -----*/
     // const msgEl = document.querySelector('h2');
-
+    const slotEls = [...document.querySelectorAll("slots > div")];
     const replayBtn = document.querySelector("button");
-    const spaceEls = [...document.querySelectorAll("#board > div")]
 
     // /*----- event listeners -----*/
-    // // event listener for click to register what column the user clicks in.
-    document.getElementById('board').addEventListener('click', handleChoice); 
+    document.getElementById('slots').addEventListener('click', handleChoice);
     replayBtn.addEventListener('click', init);
 
     /*----- functions -----*/
@@ -27,13 +25,13 @@
 
     function init() {
         board = [
-            [1, 0, 0, 0, 0, 1], // column 1
+            [0, 0, 0, 0, 0, 0], // column 1
             [0, 0, 0, 0, 0, 0], // column 2
             [0, 0, 0, 0, 0, 0], // column 3
             [0, 0, 0, 0, 0, 0], // column 4
             [0, 0, 0, 0, 0, 0], // column 5
             [0, 0, 0, 0, 0, 0], // column 6
-            [1, 0, 0, 0, 0, -1], // column 7
+            [0, 0, 0, 0, 0, 0], // column 7
         ]    
         turn = 1; 
         gameStatus = null;
@@ -62,21 +60,20 @@
     };
 
     function handleChoice(evt) {
-        // put a guard up to not all clicks in gaps and columns that are full, and spaces that have already been taken. 
-        renderDrop();
-        turn *= -1; 
-        const idx = spaceEls.indexOf(evt.target);
-        spaceValue[idx] = turn;
-        gameStatus = getGameStatus();
-
+        const columnIndex = slotEls.indexOf(evt.target);
+        if (columnIndex === -1) return; // guard so that event must happen within the slots array
+        const columnArr = board[columnIdx]; 
+        if (!columnArr.includes(0)) return;
+        // ingore a click if the column includes 
+        const rowIdx = columnArr.indexOf(0);
+        columnArr[rowIdx] = turn; 
+        turn *= -1;
+        render();
     };
     
-
-   function renderDrop() {
-    //     const idx = squareEls.indexOf(evt.target);
-
-   };
-//   function getGameStatus() {
+// get slot status 
+// if colum doesnt include 0 ? invisible : visisble 
+// //   function getGameStatus() {
 //         checkWin();
 //         if (checkWin === true) return "WIN"
 //         if !board.contains() return "T"
@@ -86,73 +83,73 @@
     //if boardincludes '0s' => return null; 
 //   }  
   
-function getGameStatus() {};
-//   function gameStatus() {
-//     checkHorizontal(board, value)
-//     || checkVertical(board, value) 
-//     || checkDiagonal(board, value)  
-//     return 'WIN'
-//     if () // game board doesn't include '0'then return 'T"
-//     else return // null
-// }
+// function getGameStatus() {};
+// //   function gameStatus() {
+// //     checkHorizontal(board, value)
+// //     || checkVertical(board, value) 
+// //     || checkDiagonal(board, value)  
+// //     return 'WIN'
+// //     if () // game board doesn't include '0'then return 'T"
+// //     else return // null
+// // }
  
 
     
-function checkVertical() {
-    for (let i = 0; i < 5; i++)
-    if ( board[0][i] === value
-        && board[1][i] === value
-        && board[2][i] === value
-        && board[3][i] === value
-        )
-        return true;
-    if ( board[1][i] === value
-        && board[2][i] === value
-        && board[3][i] === value
-        && board[4][i] === value
-        )
-        return true;
-    if (board[2][i] === value
-        && board[3][i] === value
-        && board[3][i] === value
-        &&board[4][i] === value
-        )
-        return true;
-}
+// function checkVertical() {
+//     for (let i = 0; i < 5; i++)
+//     if ( board[0][i] === value
+//         && board[1][i] === value
+//         && board[2][i] === value
+//         && board[3][i] === value
+//         )
+//         return true;
+//     if ( board[1][i] === value
+//         && board[2][i] === value
+//         && board[3][i] === value
+//         && board[4][i] === value
+//         )
+//         return true;
+//     if (board[2][i] === value
+//         && board[3][i] === value
+//         && board[3][i] === value
+//         &&board[4][i] === value
+//         )
+//         return true;
+// }
 
-function checkHorizontal(board, value) { 
-    for (let i = 0; i < 5; i++)   
-    if (board[i][0] === value
-        && board[i][1] === value
-        && board[i][2] === value
-        && board[i][3] === value
-        )
-        return true;
-    if (board[i][1] === value
-        && board[i][2] === value
-        && board[i][3] === value
-        && board[i][4] === value
-        )
-        return true;
-    if (board[i][2] === value
-        && board[i][3] === value
-        && board[i][4] === value
-        && board[i][5] === value
-        )
-        return true;
-    if (board[i][3] === value
-        && board[i][4] === value
-        && board[i][5] === value
-        && board[i][6] === value
-        )
-        return true;
-    }
+// function checkHorizontal(board, value) { 
+//     for (let i = 0; i < 5; i++)   
+//     if (board[i][0] === value
+//         && board[i][1] === value
+//         && board[i][2] === value
+//         && board[i][3] === value
+//         )
+//         return true;
+//     if (board[i][1] === value
+//         && board[i][2] === value
+//         && board[i][3] === value
+//         && board[i][4] === value
+//         )
+//         return true;
+//     if (board[i][2] === value
+//         && board[i][3] === value
+//         && board[i][4] === value
+//         && board[i][5] === value
+//         )
+//         return true;
+//     if (board[i][3] === value
+//         && board[i][4] === value
+//         && board[i][5] === value
+//         && board[i][6] === value
+//         )
+//         return true;
+//     }
 
-function checkVertical (board,){
-for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
+// function checkVertical (board,){
+// for (let i = 0; i < 5; i++) {
+//         for (let j = 0; j < 5; j++) {
 
-        }
-    }            
-};
+//         }
+//     }            
+// };
 
