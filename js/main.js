c0dbf9cab780e42b9fb87a95f33746c74d9df598
lilliesheely <lilliesheely@gmail.com>
2022-07-05 13:17:29 -0700

    stubbed up checkWin

--- js/main.js
+++ js/main.js
@@ -10,15 +10,17 @@
     let board; 
     let turn; 
     let gameStatus; 
+    let checkWin;
 
     // /*----- cached element references -----*/
     const slotEls = [...document.querySelectorAll("#slots > div")];
     const replayBtn = document.querySelector("button");
     const messageEl = document.querySelector("h2");
+    const player = board[columnIdx][rowIdx]
 
     // /*----- event listeners -----*/
     document.getElementById('slots').addEventListener('click', handleChoice);
-    //replayBtn.addEventListener('click', init);
+    replayBtn.addEventListener('click', init);
 
     /*----- functions -----*/
     init();
@@ -57,6 +59,7 @@
         // ingore a click if the column includes, CAN REMOVE THIS IF WE PUT A HANDLE MARKERS (IF NO ZEROS EXISTS, REMOVE SLOT SO THAT YOU CAN'T ADD A MOVE THERE. )
         const rowIdx = columnArr.indexOf(0);
         columnArr[rowIdx] = turn; 
+        checkWin = checkWin(columnArr,rowIdx)
         turn *= -1;
         gameStatus = getGameStatus();
         render();
@@ -72,90 +75,18 @@
         };
     };
 
+    function checkWin(colIdx, rowIdx) {
+        const player = board[colIdx][rowIdx];
+        return checkVertWin(colIdx, player) ||
+        checkHorzWin(colIdx, rowIdx, player) ||
+        checkDiagWinI(colIdx, rowIdx, player) 
+    }
     
-// get slot status 
-// if colum doesnt include 0 ? invisible : visisble 
 
-    // if there is a winner by: (figure out how to find winner) return 
-    // if board does not include 0s => return 't' 
-    //if boardincludes '0s' => return null; 
-//   }  
-  
 function getGameStatus(){
     let flatBoard = board.flat(2); 
     if (!flatBoard.includes(0)) return 't'; // game board doesn't include '0'then return 'T"
-    checkHorizontal(board);
-    if (checkHorizontal === true) return 'w';
-//     || checkVertical() 
-// //  || checkDiagonal()
-//         return 'w';
+    if (checkWin === true) return 'w'; 
    return null;
 }
     
-function checkVertical(board, value) {
-    for (let i = 0; i < 5; i++) {
-    if ( board[0][i] === value
-        && board[1][i] === value
-        && board[2][i] === value
-        && board[3][i] === value
-        )
-        return true;
-    if ( board[1][i] === value
-        && board[2][i] === value
-        && board[3][i] === value
-        && board[4][i] === value
-        )
-        return true;
-    if (board[2][i] === value
-        && board[3][i] === value
-        && board[3][i] === value
-        &&board[4][i] === value
-        )
-        return true;
-    } 
-    return false; 
-}
-
-function checkHorizontal(board, value) { 
-    for (let i = 0; i < 5; i++) {    
-    if (board[i][0] === value
-        && board[i][1] === value
-        && board[i][2] === value
-        && board[i][3] === value
-        )
-        return true;
-    if (board[i][1] === value
-        && board[i][2] === value
-        && board[i][3] === value
-        && board[i][4] === value
-        )
-        return true;
-    if (board[i][2] === value
-        && board[i][3] === value
-        && board[i][4] === value
-        && board[i][5] === value
-        )
-        return true;
-    if (board[i][3] === value
-        && board[i][4] === value
-        && board[i][5] === value
-        && board[i][6] === value
-        )
-        return true;
-    }
-    return false; 
-}
-
-// function checkVertical(board){
-// for (let i = 0; i < board.length; i++) {
-//         for (let j = 0; j < board[].le; j++) {
-//         let d = j + 1;
-//         if (board[i][j+1] === value
-//         board[i][j+1] === value
-
-//         )
-//         }
-//     }            
-// };
-
-
