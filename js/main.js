d8e01cf44a9ab7cd52cccb3ce734fad9149d6811
lilliesheely <lilliesheely@gmail.com>
2022-07-06 14:12:20 -0700

    updated getHorz & cleaned up code format

--- js/main.js
+++ js/main.js
@@ -1,68 +1,70 @@
-    // /*----- constants -----*/
-    const COLOR_LOOKUP = { 
-        "1": 'white', 
-        "-1": 'black',
-        "0": 'yellow'
-    }
+// /*----- constants -----*/
+const COLOR_LOOKUP = { 
+    "1": 'white', 
+    "-1": 'black',
+    "0": 'yellow'
+}
 
-    // /*----- app's state (variables) -----*/
-    let board; 
-    let turn; 
-    let gameStatus; 
-    let winner = null;
+    // -1 url/omages/pepperoni
+
+// /*----- app's state (variables) -----*/
+let board; 
+let turn; 
+let gameStatus; 
+let winner = null;
  
-    // /*----- cached element references -----*/
-    const slotEls = [...document.querySelectorAll("#slots > div")];
-    const replayBtn = document.querySelector("button");
-    const messageEl = document.querySelector("h2");
+// /*----- cached element references -----*/
+const slotEls = [...document.querySelectorAll("#slots > div")];
+const replayBtn = document.querySelector("button");
+const messageEl = document.querySelector("h2");
 
-    // /*----- event listeners -----*/
-    document.getElementById('slots').addEventListener('click', handleChoice);
-    replayBtn.addEventListener('click', init);
+// /*----- event listeners -----*/
+document.getElementById('slots').addEventListener('click', handleChoice);
+replayBtn.addEventListener('click', init);
 
-    /*----- functions -----*/
-    init();
+/*----- functions -----*/
+init();
 
-    function init() {
-        board = [
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-        ]    
-        turn = 1; 
-        gameStatus = null;
-        render();  
-        winner = null; 
-    }; 
+function init() {
+    board = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ]    
+    turn = 1; 
+    gameStatus = null;
+    render();  
+    winner = null; 
+}; 
 
-    function render() {
-        board.forEach(function(columnArr, columnIdx) {
-            columnArr.forEach(function(spaceValue, rowIdx) {
-                const spaceEl = document.getElementById(`c${columnIdx}r${rowIdx}`);   
-                spaceEl.style.backgroundColor = COLOR_LOOKUP[spaceValue];           
-            });
+function render() {
+    board.forEach(function(columnArr, columnIdx) {
+        columnArr.forEach(function(spaceValue, rowIdx) {
+            const spaceEl = document.getElementById(`c${columnIdx}r${rowIdx}`);   
+            spaceEl.style.backgroundColor = COLOR_LOOKUP[spaceValue];           
         });
-        renderMessage();
-        replayBtn.style.visibility = gameStatus ? 'visible' : 'hidden'; 
-    };
+    });
+    renderMessage();
+    renderSlots();
+    replayBtn.style.visibility = gameStatus ? 'visible' : 'hidden'; 
+};
 
-    function handleChoice(evt) {
-        const colIdx = slotEls.indexOf(evt.target);
-        if (colIdx === -1 || winner === true) return;
-        const columnArr = board[colIdx]; 
-        const rowIdx = columnArr.indexOf(0);
-        columnArr[rowIdx] = turn; 
-        winner = checkWin(colIdx,rowIdx);
-        console.log(winner)
-        turn *= -1;
-        gameStatus = getGameStatus();
-        renderSlots();
-        render();
-    };
+function handleChoice(evt) {
+    const colIdx = slotEls.indexOf(evt.target);
+    if (colIdx === -1 || winner === true) return;
+    const columnArr = board[colIdx]; 
+    const rowIdx = columnArr.indexOf(0);
+    columnArr[rowIdx] = turn; 
+    winner = checkWin(colIdx,rowIdx);
+    console.log(winner)
+    turn *= -1;
+    gameStatus = getGameStatus();
+    render();
+};
 
 function renderMessage(player){
         if (gameStatus === null) {
@@ -77,7 +79,7 @@ function renderMessage(player){
 function checkWin(colIdx, rowIdx) {
         const player = board[colIdx][rowIdx];
         return checkVertWin(colIdx, rowIdx, player) || 
-        checkHorzWin(colIdx, rowIdx, player) ||
+        checkHorzWin(colIdx, rowIdx) ||
         checkDiagWinLeft(colIdx, rowIdx) ||
         checkDiagWinRight(colIdx, rowIdx);
 };
@@ -92,86 +94,79 @@ function checkVertWin(colIdx, rowIdx, player) {
         count++;
         rowIdx--;
     }    
-  return count === 4 ? winner = true : null // here
-}
+  return count === 4 ? winner = true : null; 
+};
     
-function checkHorzWin(colIdx, rowIdx, player) {
-        for(i = 0; i < 5; ++i){
-          if (board[0][i] === player 
-              && board[1][i] === player
-              && board[2][i] === player
-              && board[3][i] === player
-          ) return true;
-          if (board[1][i] === player 
-              && board[2][i] === player
-              && board[3][i] === player
-              && board[4][i] === player
-          ) return true;
-          if (board[2][i] === player 
-              && board[3][i] === player
-              && board[4][i] === player
-              && board[5][i] === player
-          ) return true;
-          if (board[3][i] === player 
-              && board[4][i] === player
-              && board[5][i] === player
-              && board[6][i] === player
-          ) return true;
-        } 
+function checkHorzWin(colIdx, rowIdx) {
+    const player = board[colIdx][rowIdx];
+    let count = 1; 
+    let idx = colIdx + 1; 
     
+    while (idx < board.length && board[idx][rowIdx] === player) { 
+        count++; 
+        idx++; 
+    }
+    idx = colIdx - 1; 
+    while (idx >= 0 && board[idx][rowIdx] === player) {
+        count++; 
+        idx--; 
+    } 
+    return count >= 4 ? winner = true : null;
 };
 
 
 function checkDiagWinLeft(colIdx, rowIdx) {
-        const player = board[colIdx][rowIdx];
-        let count = 1;
-        let idx1 = colIdx - 1; 
-        let idx2 = rowIdx + 1;  
-     
-        while (idx1 >= 0 && idx2 < board[0].length && board[idx1][idx2] === player) {
-            count++; 
-            idx1--;
-            idx2++;
-        }
-        idx1 = colIdx + 1;
-        idx2 = rowIdx - 1;
-        while (idx1 < board.length && idx2 >= 0 && board[idx1][idx2] === player) {
-            count++;
-            idx1++;
-            idx2--; 
-        }
-        return count >= 4 ? winner = true : null  
+    const player = board[colIdx][rowIdx];
+    let count = 1;
+    let idx1 = colIdx - 1; 
+    let idx2 = rowIdx + 1;  
+    
+    while (idx1 >= 0 && idx2 < board[0].length && board[idx1][idx2] === player) {
+        count++; 
+        idx1--;
+        idx2++;
     }
-    function checkDiagWinRight(colIdx, rowIdx) {
-        const player = board[colIdx][rowIdx];
-        let count = 1;
-        let idx1 = colIdx + 1; 
-        let idx2 = rowIdx + 1;  
-     
-        while (idx1 < board.length && idx2 < board[0].length && board[idx1][idx2] === player) {
-            count++; 
-            idx1++;
-            idx2++;
-        }
-        idx1 = colIdx - 1;
-        idx2 = rowIdx - 1;
-        while (idx1 >= 0 && idx2 >= 0 && board[idx1][idx2] === player) {
-            count++;
-            idx1--;
-            idx2--; 
-        }
-        return count >= 4 ? winner = true : null 
+    
+    idx1 = colIdx + 1;
+    idx2 = rowIdx - 1;
+    while (idx1 < board.length && idx2 >= 0 && board[idx1][idx2] === player) {
+        count++;
+        idx1++;
+        idx2--; 
     }
+    return count >= 4 ? winner = true : null;  
+};
+function checkDiagWinRight(colIdx, rowIdx) {
+    const player = board[colIdx][rowIdx];
+    let count = 1;
+    let idx1 = colIdx + 1; 
+    let idx2 = rowIdx + 1;  
+    
+    while (idx1 < board.length && idx2 < board[0].length && board[idx1][idx2] === player) {
+        count++; 
+        idx1++;
+        idx2++;
+    }
+
+    idx1 = colIdx - 1;
+    idx2 = rowIdx - 1;
+    while (idx1 >= 0 && idx2 >= 0 && board[idx1][idx2] === player) {
+        count++;
+        idx1--;
+        idx2--; 
+    }
+    return count >= 4 ? winner = true : null; 
+};
        
 function getGameStatus() {
     let flatBoard = board.flat(2); 
     if (!flatBoard.includes(0)) return 't'; 
     if (winner === true) return 'w'; 
-   return null;
-}
+    return null;
+};
 
 function renderSlots () {
     slotEls.forEach(function(slotEl, colIdx) {
         slotEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden'; 
     });
-}
+};
