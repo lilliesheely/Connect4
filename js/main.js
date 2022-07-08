3e9625b1976a4060bc1244b674d0fdd94aaa40a4
lilliesheely <lilliesheely@gmail.com>
2022-07-07 19:22:14 -0700

    updated css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // /*----- constants -----*/
 const IMAGE_LOOKUP = { 
     "1": "url(https://i.imgur.com/YJOzxFl.png?2)", 
-    "-1": "url(https://i.imgur.com/uenhlcC.png?1)",
+    "-1": "url(https://i.imgur.com/uenhlcC.png?3)",
     "0": ''
 }
 
